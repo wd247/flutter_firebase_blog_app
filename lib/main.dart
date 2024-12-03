e57780b11a17c2c6c8f83e0b58290714fced3848
wd247 <junny3344@gmail.com>
2024-12-03 09:37:53 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_firebase_blog_app/ui/home/home_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+//test
 void main() {
   runApp(ProviderScope(child: MyApp()));
 }
