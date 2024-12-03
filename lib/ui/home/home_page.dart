b0c1c3ed95fe6d4d521f1ba2e038e03d228f6a6f
wd247 <junny3344@gmail.com>
2024-12-03 12:08:49 +0900

    홈페이지 ui 하단 날짜시간까지 입력

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -7,6 +7,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('BLOG'),
       ),
+      backgroundColor: Colors.grey[200],
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Column(
@@ -21,12 +22,70 @@ class HomePage extends StatelessWidget {
             ),
             SizedBox(height: 20),
             Expanded(
+                // 리스트뷰 재활용 해야하니 함수로 빼서~
                 child: ListView(
               children: [
                 Container(
                   width: double.infinity,
                   height: 120,
-                  color: Colors.blue,
+                  child: Stack(
+                    children: [
+                      //
+                      Positioned(
+                        right: 0,
+                        width: 120,
+                        height: 120,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.network(
+                            'https://picsum.photos/200/300',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        margin: EdgeInsets.only(right: 100),
+                        padding: EdgeInsets.all(20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Today I Learned',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                              ),
+                            ),
+                            Spacer(),
+                            Text(
+                              'flutter 그리드뷰를 배웠습니다.flutter 그리드뷰를 배웠습니다.flutter 그리드뷰를 배웠습니다.',
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 12,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 4,
+                            ),
+                            Text(
+                              '2024.12.03 12:02',
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             )),
