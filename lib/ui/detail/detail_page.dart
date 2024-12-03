cd7d047c3eddd7a6c7022c81fa8b786590ac7b4c
wd247 <junny3344@gmail.com>
2024-12-03 21:18:24 +0900

    ㅇㅇ

--- lib/ui/detail/detail_page.dart
+++ lib/ui/detail/detail_page.dart
@@ -1,17 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_blog_app/ui/write/write_page.dart';
 
 class DetailPage extends StatelessWidget {
   @override
-  Widget build(Object context) {
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Text('DetailPage'),
+      appBar: AppBar(
+        actions: [
+          iconButton(Icons.delete, () {
+            print('삭제 아이콘 터치함');
+          }),
+          iconButton(Icons.edit, () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) {
+                return WritePage();
+              }),
+            );
+          }),
+        ],
+      ),
+      body: ListView(
+        padding: EdgeInsets.only(bottom: 400),
+        children: [
+          Image.network(
+            'https://picsum.photos/200/300',
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Today I Learned',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                SizedBox(
+                  height: 14,
+                ),
+                Text(
+                  '이지원',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                Text(
+                  '2024.12.03 16:58',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w200,
+                  ),
+                ),
+                SizedBox(
+                  height: 14,
+                ),
+                Text(
+                  'Flutter 의 그리드뷰를 배웠습니다.' * 10,
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 
-  Widget iconButton() {
+  Widget iconButton(IconData icon, void Function() onTap) {
     return GestureDetector(
-      child: Icon(Icons.delete),
+      onTap: onTap,
+      child: Container(
+        width: 50,
+        height: 50,
+        color: Colors.transparent,
+        child: Icon(icon),
+      ),
     );
   }
 }
