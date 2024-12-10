e27e5ad68643ab85e048808399fd5801c1031745
wd247 <junny3344@gmail.com>
2024-12-10 22:39:37 +0900

    하루끝 저장

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase_blog_app/data/post_repository.dart';
 import 'package:flutter_firebase_blog_app/firebase_options.dart';
 import 'package:flutter_firebase_blog_app/ui/home/home_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -9,14 +8,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  final postRepo = PostRepository();
-  await postRepo.insert(
-    title: 'title',
-    content: '내용입니다',
-    writer: '오상구',
-    imageUrl: 'https://picsum.photos/200/300',
   );
 
   runApp(ProviderScope(child: MyApp()));
